359ce753efee4b565db4a2c09c5ea3295355cf26
Adamniels <adamnielsen401@gmail.com>
2025-04-05 09:27:48 +0200

    fixed sqrew up

--- react-portfolio/src/App.tsx
+++ react-portfolio/src/App.tsx
@@ -1,12 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Experience from "./pages/Experience"
-import TechnicalExperience from "./pages/TechnicalExp"
-
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Experience from "./pages/Experience";
+import TechnicalExperience from "./pages/TechnicalExp";
 
 function App() {
   return (
@@ -17,7 +16,8 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/technicalExperience" element={<TechnicalExperience />} />
         <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Layout>
   );
